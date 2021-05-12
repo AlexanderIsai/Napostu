ab8bc768a4d93064af562107f28f1c4ec4c338a0
Viktoriya Voikshnaras <viktoriya_sofia@yahoo.com>
2021-05-12 09:45:45 +0300

    fixed conflict

---
+++ front/src/components/Avatar/Avatar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Avatar.scss"
+
+const Avatar = (props) => {
+  const {url} = props
+  return (
+    <div className="avatar">
+      <img className="avatar-img" src={url} alt="beard"/>
+    </div>
+  );
+};
+
+export default Avatar;
