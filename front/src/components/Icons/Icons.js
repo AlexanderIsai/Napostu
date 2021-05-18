0a41721dd32b0312c6c79a8b06f9a781c1759ec1
Viktoriya Voikshnaras <viktoriya_sofia@yahoo.com>
2021-05-18 15:25:41 +0000

    Update Icons.js

--- front/src/components/Icons/Icons.js
+++ front/src/components/Icons/Icons.js
@@ -1,7 +1,53 @@
 import React from 'react';
 import IconButton from "@material-ui/core/IconButton";
 import Tooltip from '@material-ui/core/Tooltip';
-import {useStyles, MyAddCommentIcon, MyThumbUpIcon, MyFavoriteIcon} from './IconsStyle';
+import {useStyles, MyAddCommentIcon, MyThumbUpIcon, MyFavoriteIconPassive, MyFavoriteIconActive} from './IconsStyle';
+import {createMuiTheme} from "@material-ui/core/styles";
+import {ThemeProvider} from "@material-ui/styles";
+
+const theme = createMuiTheme({
+  props: {
+    MuiButtonBase: {
+      disableRipple: true,
+    },
+  },
+});
+
+
+export const IFavoritePost = (props) => {
+  const {onClick, isFavorite} = props;
+  const handleClick = () => {
+    onClick()
+  }
+
+  const classes = useStyles();
+  return (
+    <>
+      {(isFavorite) &&
+      <ThemeProvider theme={theme}>
+        <IconButton aria-label="add favorite" variant="contained" color="primary" className={classes.iconStile}>
+          <Tooltip title="remove" arrow placement="top-start">
+            <MyFavoriteIconActive
+              onClick={handleClick}
+            />
+          </Tooltip>
+        </IconButton>
+      </ThemeProvider>
+      }
+      {(!isFavorite) &&
+      <ThemeProvider theme={theme}>
+        <IconButton aria-label="add favorite" variant="contained" color="primary" className={classes.iconStile}>
+          <Tooltip title="add to favorites" arrow placement="top-start">
+            <MyFavoriteIconPassive
+              onClick={handleClick}
+            />
+          </Tooltip>
+        </IconButton>
+      </ThemeProvider>
+      }
+    </>
+  )
+};
 
 
 export const ILikePost = (props) => {
@@ -13,34 +59,15 @@ export const ILikePost = (props) => {
   const classes = useStyles();
   return (
     <>
-      <IconButton aria-label="add favorite" variant="contained" color="primary" className={classes.iconStile}>
-        <Tooltip title="like" arrow placement="top-start">
-          <MyThumbUpIcon
-            onClick={handleClick}
-          />
-        </Tooltip>
-      </IconButton>
-    </>
-  )
-};
-
-
-export const IFavoritePost = (props) => {
-  const {onClick} = props;
-  const handleClick = () => {
-    onClick()
-  }
-
-  const classes = useStyles();
-  return (
-    <>
-      <IconButton aria-label="add favorite" variant="contained" color="primary" className={classes.iconStile}>
-        <Tooltip title="add favorites" arrow placement="top-start">
-          <MyFavoriteIcon
-            onClick={handleClick}
-          />
-        </Tooltip>
-      </IconButton>
+      <ThemeProvider theme={theme}>
+        <IconButton aria-label="add favorite" variant="contained" color="primary" className={classes.iconStile}>
+          <Tooltip title="like" arrow placement="top-start">
+            <MyThumbUpIcon
+              onClick={handleClick}
+            />
+          </Tooltip>
+        </IconButton>
+      </ThemeProvider>
     </>
   )
 };
@@ -55,14 +82,15 @@ export const IAddComment = (props) => {
   const classes = useStyles();
   return (
     <>
-      <IconButton aria-label="add comment" variant="contained" color="primary" className={classes.iconStile}>
-        <Tooltip title="add comments" arrow placement="top-start">
-          <MyAddCommentIcon
-            onClick={handleClick}
-          />
-        </Tooltip>
-      </IconButton>
+      <ThemeProvider theme={theme}>
+        <IconButton aria-label="add comment" variant="contained" color="primary" className={classes.iconAddComment}>
+          <Tooltip title="place comments" arrow placement="left-start">
+            <MyAddCommentIcon
+              onClick={handleClick}
+            />
+          </Tooltip>
+        </IconButton>
+      </ThemeProvider>
     </>
   )
 };
-
