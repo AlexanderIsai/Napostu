37d0e6e098afbc66e208c40b67c19d71d3b79282
Viktoriya Voikshnaras <viktoriya_sofia@yahoo.com>
2021-05-18 00:19:30 +0000

    Update MainPageStyles.js

--- front/src/pages/MainPage/MainPageStyles.js
+++ front/src/pages/MainPage/MainPageStyles.js
@@ -1,22 +1,25 @@
 import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    position: "relative",
-    maxWidth: "1280px",
-    margin: " 0 auto",
-    padding: "0px 24px 0 24px",
-    height: "100vh",
-    overflow: "scroll",
-    textAlign: "center",
-    background: "linear-gradient(90deg, rgba(240,241,242,1) 0%, " +
-      "rgba(253,253,253,1) 25%, rgba(255,255,255,1) 50%, rgba(254,254,254,1) 75%, " +
-      "rgba(240,241,242,1) 100%)",
+    root: {
+      flexGrow: 1,
+      position: "relative",
+      maxWidth: "1280px",
+      margin: " 0 auto",
+      height: "100vh",
+      overflow: "scroll",
+      textAlign: "center",
+      background: "linear-gradient(90deg, rgba(240,241,242,1) 0%, " +
+        "rgba(253,253,253,1) 25%, rgba(255,255,255,1) 50%, rgba(254,254,254,1) 75%, " +
+        "rgba(240,241,242,1) 100%)",
 
-  },
+    },
 
-  title: {
+    paddingBox: {
+      padding: "0px 24px 0 24px",
+    },
+
+    title: {
       fontSize: "2em",
       fontWeight: "500",
       letterSpacing: "-0.04em",
@@ -49,16 +52,16 @@ const useStyles = makeStyles((theme) => ({
       fontSize: "0.7em"
     },
 
-  linkToSubscribe:{
-    textDecoration: "none",
-    fontSize: "1.2em",
-    color: "#f99f38",
-    fontWeight: "500",
-    lineHeight: "0.7",
-    letterSpacing: "0.027em",
-    margin: 0,
-    fontVariant: "all-small-caps",
-  },
+    linkToSubscribe: {
+      textDecoration: "none",
+      fontSize: "1.2em",
+      color: "#f99f38",
+      fontWeight: "500",
+      lineHeight: "0.7",
+      letterSpacing: "0.027em",
+      margin: 0,
+      fontVariant: "all-small-caps",
+    },
 
     subscribtionElBoxSticky: {
       position: "sticky",
@@ -143,15 +146,17 @@ const useStyles = makeStyles((theme) => ({
       margin: "0 8px",
       color: "#ffffff",
       background: "#ffd54b",
+      cursor: "pointer",
     },
 
     activeUserTxt: {
-      fontSize: "0.9em",
-      fontWeight: "500",
+      fontSize: "0.86em",
+      fontWeight: "400",
       fontVariant: "small-caps",
-      lineHeight: "1.18",
+      lineHeight: "0.96",
       letterSpacing: "0.027em",
       margin: 0,
+      padding: "4px",
     },
 
     activeUserAvatarBox: {
@@ -197,8 +202,9 @@ const useStyles = makeStyles((theme) => ({
       textDecoration: "none",
       fontSize: "0.7em",
       color: "#000000",
-    }
-  }))
-;
+    },
+
+  })
+);
 
 export default useStyles;
